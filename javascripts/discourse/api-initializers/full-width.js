0229f0d341d660946f85beaf5dfce13111612d40
awesomerobot <kris.aubuchon@discourse.org>
2023-02-03 16:33:32 -0500

    EXPERIMENTAL: make better use of space w/ logos

--- javascripts/discourse/api-initializers/full-width.js
+++ javascripts/discourse/api-initializers/full-width.js
@@ -1,11 +1,20 @@
 import { apiInitializer } from "discourse/lib/api";
 import hbs from "discourse/widgets/hbs-compiler";
+import Session from "discourse/models/session";
+import { h } from "virtual-dom";
+import { iconNode } from "discourse-common/lib/icon-library";
 
 export default apiInitializer("0.8", (api) => {
   api.reopenWidget("header-contents", {
     tagName: "div.contents",
+
+    buildClasses(attrs) {
+      return attrs.minimized ? "header-title-docked" : "";
+    },
+
     template: hbs`
-      <div class="header-contents__toggle-and-logo">
+   
+      <div class="header-contents__toggle-and-logo" data-docked-title={{attrs.minimized}}>
         {{#if this.site.desktopView}}
           {{#if attrs.sidebarEnabled}}
             {{sidebar-toggle attrs=attrs}}
@@ -19,5 +28,51 @@ export default apiInitializer("0.8", (api) => {
       
       <div class="panel clearfix" role="navigation">{{yield}}</div>
     `,
+  });
+
+  api.reopenWidget("home-logo", {
+    logo() {
+      const { siteSettings } = this,
+        mobileView = this.site.mobileView;
+
+      const darkModeOptions = Session.currentProp("darkModeAvailable")
+        ? { dark: true }
+        : {};
+
+      const mobileLogoUrl = this.mobileLogoUrl(),
+        mobileLogoUrlDark = this.mobileLogoUrl(darkModeOptions);
+
+      const showMobileLogo = mobileView && mobileLogoUrl.length > 0;
+
+      const logoUrl = this.logoUrl(),
+        logoUrlDark = this.logoUrl(darkModeOptions);
+      const title = siteSettings.title;
+
+      if (this.attrs.minimized && !this.attrs.showSidebar) {
+        const logoSmallUrl = this.smallLogoUrl(),
+          logoSmallUrlDark = this.smallLogoUrl(darkModeOptions);
+        if (logoSmallUrl.length) {
+          return this.logoElement(
+            "logo-small",
+            logoSmallUrl,
+            title,
+            logoSmallUrlDark
+          );
+        } else {
+          return iconNode("home");
+        }
+      } else if (showMobileLogo) {
+        return this.logoElement(
+          "logo-mobile",
+          mobileLogoUrl,
+          title,
+          mobileLogoUrlDark
+        );
+      } else if (logoUrl.length) {
+        return this.logoElement("logo-big", logoUrl, title, logoUrlDark);
+      } else {
+        return h("h1#site-text-logo.text-logo", { key: "logo-text" }, title);
+      }
+    },
   });
 });
